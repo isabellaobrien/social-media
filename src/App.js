bebd16a21e9f002af4c184ffc6a73e2f1d16921b
isabellaobrien <bellaobrien.it@gmail.com>
2024-01-14 19:00:23 +0000

    Add routing to NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import NavBar from "./components/NavBar";
 import styles from "./App.module.css";
+import { Route, Switch } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>}></Route>
+          <Route exact path="/sign-in" render={() => <h1>sign in</h1>}></Route>
+          <Route exact path="/sign-up" render={() => <h1>sign up</h1>}></Route>
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import {Navbar, Nav, NavDropdown, Container} from 'react-bootstrap';
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="lg">
         <Container>
-            <Navbar.Brand className={styles.logo}>Logo</Navbar.Brand>
+            <NavLink to="/">
+              <Navbar.Brand className={styles.logo}>Logo</Navbar.Brand>
+            </NavLink>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                <Nav.Link className={styles.link}>Home</Nav.Link>
-                <Nav.Link className={styles.link}>Link</Nav.Link>
+                <NavLink 
+                  exact 
+                  to="/" 
+                  className={styles.link} 
+                  activeClassName={styles.active}
+                >Home</NavLink>
+                <NavLink 
+                  to="/sign-in" 
+                  className={styles.link} 
+                  activeClassName={styles.active}
+                >sign in</NavLink>
+                <NavLink 
+                  to="/sign-up" 
+                  className={styles.link} 
+                  activeClassName={styles.active}
+                >sign up</NavLink>
                 <NavDropdown title="More" id="basic-nav-dropdown">
                     <NavDropdown.Item>Action</NavDropdown.Item>
                     <NavDropdown.Item>Another action</NavDropdown.Item>
