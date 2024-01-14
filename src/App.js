8f8f5a3694216509bd8e4195da8328c00d3bdc2a
isabellaobrien <bellaobrien.it@gmail.com>
2024-01-14 19:33:02 +0000

    Connect project to api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import styles from "./App.module.css";
 import { Route, Switch } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import "./api/axiosDefault";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Home</h1>}></Route>
           <Route exact path="/sign-in" render={() => <h1>sign in</h1>}></Route>
           <Route exact path="/sign-up" render={() => <h1>sign up</h1>}></Route>
+          <Route render={() => <h1>Page not found</h1>}></Route>
         </Switch>
       </Container>
     </div>
