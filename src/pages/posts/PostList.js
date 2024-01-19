e2d9dd1f9e1d412e682213e95ef0cf142485f6c7
isabellaobrien <bellaobrien.it@gmail.com>
2024-01-19 22:58:28 +0000

    Add search bar to PostList component

--- src/pages/posts/PostList.js
+++ src/pages/posts/PostList.js
@@ -1,5 +1,5 @@
 import React, {  useEffect,useState } from 'react'
-import {Row, Col, Container} from "react-bootstrap"
+import {Row, Col, Container, Form} from "react-bootstrap"
 import { useLocation } from 'react-router';
 import { axiosReq } from '../../api/axiosDefault';
 import Post from './Post';
@@ -11,11 +11,12 @@ const PostList = ({ message, filter = "" }) => {
     const [posts, setPosts] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
     const { pathname } = useLocation();
-  
+    
+    const [query, setQuery] = useState("")
     useEffect(() => {
       const fetchPosts = async () => {
         try {
-          const { data } = await axiosReq.get(`/posts/?${filter}`);
+          const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
           setPosts(data);
           setHasLoaded(true);
         } catch (err) {
@@ -24,13 +25,31 @@ const PostList = ({ message, filter = "" }) => {
       };
   
       setHasLoaded(false);
-      fetchPosts();
-    }, [filter, pathname]);
+      const timer = setTimeout(() => {
+        fetchPosts();
+      }, 1000)
+
+      return () => {
+        fetchPosts();
+        clearTimeout(timer);
+      }
+      
+    }, [filter, query, pathname]);
   
   return (
     <Row>
         <Col className="py-2 p-0 p-lg-2" lg={8}>
             <p>popular profiles to follow on mobile</p>
+            {/* <i class="fa-solid fa-magnifying-glass"></i> */}
+            <Form onSubmit={(event) => event.preventDefault()}>
+              <Form.Control
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+              type="text"
+              placeholder='search posts'
+              className="mr-sm-2"
+              />
+            </Form>
             <Container>
                 {hasLoaded? (
                 <>
